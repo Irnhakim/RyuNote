9da826c61ab03dbf03d41606b85c02f3c9da6a26
Hakim <shanun036@gmail.com>
2023-05-28 16:03:09 +0700

    rombak Layout Menu

--- app/src/main/java/com/ryunote/app/tambah.java
+++ app/src/main/java/com/ryunote/app/tambah.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class CatatanHarian extends AppCompatActivity {
+public class tambah extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_catatan_harian);
+        setContentView(R.layout.activity_tambah);
     }
 }
