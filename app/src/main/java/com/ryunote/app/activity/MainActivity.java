8d1b1f0507643a88ca1293e3dfe293ed5dda7d49
Hakim <shanun036@gmail.com>
2023-05-29 22:23:23 +0700

    tinggal view pager

--- app/src/main/java/com/ryunote/app/activity/MainActivity.java
+++ app/src/main/java/com/ryunote/app/activity/MainActivity.java
@@ -1,80 +1,38 @@
 package com.ryunote.app.activity;
 
-import android.annotation.SuppressLint;
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.ryunote.app.R;
-
-
-import com.ryunote.app.ui.info.InfoFragment;
-import com.ryunote.app.ui.note.NoteFragment;
-import com.ryunote.app.ui.profile.ProfileFragment;
-
+import com.ryunote.app.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
-    private BottomNavigationView bottomNavigationView;
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
-        getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
 
-        // Menampilkan halaman yang pertama muncul
-        getFragmentPage(new InfoFragment());
-
-        // Inisialisasi bottom nav
-        bottomNavigationView = findViewById(R.id.bottomNavigationView);
-        bottomNavigationView.setOnItemSelectedListener(new BottomNavigationView.OnItemSelectedListener() {
-            @SuppressLint("NonConstantResourceId")
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                Fragment fragment = null;
-                int itemId = item.getItemId();
-                if (itemId == R.id.info){
-                    fragment = new InfoFragment();
-                } else if (itemId == R.id.note) {
-                    fragment = new NoteFragment();
-                }else if ((itemId == R.id.profile)){
-                    fragment = new ProfileFragment();
-                }
-                return getFragmentPage(fragment);
-            }
-        });
+        BottomNavigationView navView = findViewById(R.id.nav_view);
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
+                R.id.navigation_info, R.id.navigation_note, R.id.navigation_profile)
+                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
+        NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
+        NavigationUI.setupWithNavController(binding.navView, navController);
     }
 
-    private boolean getFragmentPage(Fragment fragment) {
-        if (fragment != null) {
-            getSupportFragmentManager()
-                    .beginTransaction()
-                    .replace(R.id.page_container, fragment)
-                    .commit();
-            return true;
-        }
-        return false;
-    }
-
-    private void openFragment(Fragment fragment) {
-        if (fragment != null) {
-            getSupportFragmentManager()
-                    .beginTransaction()
-                    .replace(R.id.page_container, fragment)
-                    .commit();
-        }
-    }
 }
